735ce77f6e2bc712c97e3c1e4ef172e8fa2f8aca
Asad-ur-Rehman9998 <munkirasad@gmail.com>
2025-04-14 12:49:03 +0500

    new file

---
+++ conversions.js
@@ -0,0 +1,33 @@
+let score = "33"
+
+
+console.log (typeof score) // to check type 
+
+
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber)
+
+
+
+// "33" => 33 can easyly converted
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+let isLoggenIn = "1"
+let booleanIsLoggendIn = Boolean(isLoggenIn)
+
+console.log(booleanIsLoggendIn)
+
+
+// 1 => true
+// 0 => false
+// " " => true
+
+
+
+let someumber =  33
+ let stringNumber = String(someumber)
+
+ console.log(typeof stringNumber)
