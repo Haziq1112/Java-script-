0f67bf6074872bd5f14f10242cdc7ac11e0c7ed8
Asad-ur-Rehman9998 <munkirasad@gmail.com>
2025-04-14 12:20:24 +0500

    new file

---
+++ 01_class.js
@@ -0,0 +1,14 @@
+//variable 
+const accountID = 1122  //cosnt can not be change
+let accountEmail = "haziq@gmail.com" //let can be change
+var accountPassword = "1234" //var can be change
+let accountCity = "jahanian" //var can be change and we not using var in any where 
+let accountState; // if we don't assign any value then it will be undefined
+
+console.log(accountEmail); // simple way for single output 
+console.table([accountEmail, accountPassword, accountCity, accountState]); //for multiple output
+
+// Datatypes 
+
+
+
