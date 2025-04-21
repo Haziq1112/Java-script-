1c9c0c58660797b5b153e2b270e4119c613828e4
Asad-ur-Rehman9998 <munkirasad@gmail.com>
2025-04-21 13:08:02 +0500

    new file

---
+++ date.js
@@ -0,0 +1,17 @@
+// let myDate= new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+
+// let mycreatedDate = new Date(2025 , 3, 21)
+// let mycreatedDate = new Date("04-21-2025")
+// let mycreatedDate =  Date.now()
+
+// console.log(Date.now())
+
+
+
+let newDate = new Date()
+console.log(newDate.getDate()) // have different types by usinng newDate.
+console.log(`Today date is ${newDate.getDate} and now the time is ${newDate.getTime}`)
